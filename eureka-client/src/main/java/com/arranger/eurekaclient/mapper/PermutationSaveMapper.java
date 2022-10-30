4c12b428a04f2d16240392e5ebf986a71111ed27
Ostap Klymko <oostapk@gmail.com>
2022-10-30 23:34:59 +0200

    create mappers to map entities and dtos to each other

---
+++ eureka-client/src/main/java/com/arranger/eurekaclient/mapper/PermutationSaveMapper.java
@@ -0,0 +1,14 @@
+package com.arranger.eurekaclient.mapper;
+
+import com.arranger.eurekaclient.dto.PermutationDTO;
+import com.arranger.eurekaclient.dto.PermutationSaveDTO;
+import org.mapstruct.Builder;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring", builder = @Builder(disableBuilder = true))
+public interface PermutationSaveMapper {
+
+    PermutationDTO saveDtoToDto(PermutationSaveDTO permutationSaveDTO);
+    PermutationSaveDTO dtoToSaveDto(PermutationDTO permutationDTO);
+}
+
